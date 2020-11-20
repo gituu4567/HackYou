74087a5f60847cdf46acaa3ede8510ffa7e3376f
rokSkr <68292269+rokSkr@users.noreply.github.com>
2020-11-20 15:46:03 +0300

    Update HackYou(Termux).py

--- HackYou(Termux).py
+++ HackYou(Termux).py
@@ -1,5 +1,9 @@
 lang = "Русс"
+#open file?
+#открыть файл?
 a = input("Открыть файл(Y-N): ")
+#Menu HackYou
+#Меню HackYou
 if a == "Y":
     print("HackYou.(V.OTTT)")
     print("*Progi*-Проги(5)")
@@ -10,11 +14,19 @@ if a == "Y":
 if komm == "Progi":
     print("")
     print("Zphisher(Фишинг страниц)")
+    print("NGROK(Нужен для фишинга)")
+    print("Instashell(Брутфуст страниц инстаграмма)")
     print("Пример ввода комманды: Progi")
     print("");
     pr = input("ВВедите комманду: ");
+#all programs
+#все программы
 if pr == "Zphisher":
     print("git clone https://github.com/htr-tech/zphisher")
+if pr == "Instashell":
+    print("Git clone https://github.com/maxrooted/instashell")
+#Menu options
+#Меню настроек
 if komm == "Options":
     print("Скрипт-программа `HackYou` для `Termux`");
     print("Язык(Language): " + lang);
@@ -26,11 +38,15 @@ if enter == "Exit":
     print("*Options*-Настройки и как пользоваться")
     print("Пример ввода комманды: Progi")
     print("")
+#edit language
+#изменить язык
 if enter == "Releng":
     print("Engl, Русс");
     print("Выберите язык")
     rl = input("Какой язык вы выбрали: ")
 if rl == "Engl":
-    print("ERROR001:This language is not supported"); 
+    print("ERROR001:This language is not supported");
+#exit to programms
+#выход из программы
 if a == "N":
     print("Exit HackYou");
